3591c3333d93986e9d2adf145baf5a20ef571547
Daniel de Oquiñena <daniel.oquinena@safespring.com>
2022-12-02 11:38:35 +0100

    refactor

--- day02/day02.go
+++ day02/day02.go
@@ -6,50 +6,28 @@ import (
 	"strings"
 )
 
-func part1(r string) int {
+func scores(r string) (int, int) {
 	line := strings.Split(r, "\n")
-	score := 0
+	score1, score2 := 0, 0
 
-	possibleScores := map[string]int{
-		"A X": 4,
-		"A Y": 8,
-		"A Z": 3,
-		"B X": 1,
-		"B Y": 5,
-		"B Z": 9,
-		"C X": 7,
-		"C Y": 2,
-		"C Z": 6,
+	possibleScores := map[string]struct{ a, b int }{
+		"A X": {4, 3},
+		"A Y": {8, 4},
+		"A Z": {3, 8},
+		"B X": {1, 1},
+		"B Y": {5, 5},
+		"B Z": {9, 9},
+		"C X": {7, 2},
+		"C Y": {2, 6},
+		"C Z": {6, 7},
 	}
 
 	for _, v := range line {
-		score += possibleScores[v]
+		score1 += possibleScores[v].a
+		score2 += possibleScores[v].b
 	}
 
-	return score
-}
-
-func part2(r string) int {
-	line := strings.Split(r, "\n")
-	score := 0
-
-	possibleScores := map[string]int{
-		"A X": 3,
-		"A Y": 4,
-		"A Z": 8,
-		"B X": 1,
-		"B Y": 5,
-		"B Z": 9,
-		"C X": 2,
-		"C Y": 6,
-		"C Z": 7,
-	}
-
-	for _, v := range line {
-		score += possibleScores[v]
-	}
-
-	return score
+	return score1, score2
 }
 
 func main() {
@@ -59,8 +37,7 @@ func main() {
 		fmt.Println(err)
 	}
 
-	score1 := part1(string(f))
-	score2 := part2(string(f))
+	score1, score2 := scores(string(f))
 
 	fmt.Printf("(PART1) Total score: %v\n(PART2) Total score: %v\n", score1, score2)
 }
